21215f7037c56f63ae3d0e7bfe96d7d443b36ee0
sandbox <152235890+sandb0-0x@users.noreply.github.com>
2023-12-09 12:52:47 -0500

    Create simple template directory for Advent of Code problems

---
+++ day_template/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+use regex::Regex;
+
+fn part_one() -> () {}
+
+fn part_two() -> () {}
+
+fn main() {
+    let file_path = "input.txt";
+    let file_contents = fs::read_to_string(file_path).expect("Unable to read file: {file_path}");
+}
